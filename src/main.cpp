664bd6044e4600e8039e12b5c649e95f031e217c
jonitinTm <jonathanbermello2007@gmail.com>
2026-03-02 10:02:13 +0100

    assets

--- src/main.cpp
+++ src/main.cpp
@@ -11,12 +11,14 @@ int main(void)
 {
     // Initialization
     //--------------------------------------------------------------------------------------
-    const int screenWidth = 800;
-    const int screenHeight = 450;
+    const int screenWidth = 875;
+    const int screenHeight = 950;
+
 
     //Creatin of the character
     // Creacion del personaje a partir de un cuadrado {x, y, width, height}
-    Rectangle player = { 100, 350, 30, 30 };
+    Rectangle player = { 100, 150, 30, 30 };
+
     // Velocidad del jugador al presionar alguna tecla
     float playerSpeed = 4.0f;
     // Ahora vamos a  definir las variables de salto (Tener en cuenta que para ir hacia arriba del eje Y es nagativo y para abajo el positivo
@@ -29,9 +31,7 @@ int main(void)
     bool isJumping = false;
     //creacion del suelo {x, y, ancho, alto}
     Rectangle ground = { 0, 400, 800, 50 };
-        
- 
-
+    
 
 
 
@@ -41,8 +41,28 @@ int main(void)
 
     InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
 
+    Texture2D imgMarioIdle = LoadTexture("Assets/Textures/Characters/Mario/Dk_Mario_Idle1.png");
+    Texture2D imgMarioWalk1 = LoadTexture("Assets/Textures/Characters/Mario/Dk_Mario_Walk1.png");
+    Texture2D imgMarioWalk2 = LoadTexture("Assets/Textures/Characters/Mario/Dk_Mario_Walk2.png");
+    Texture2D imgMarioJump = LoadTexture("Assets/Textures/Characters/Mario/Dk_Mario_Jump.png");
+    Vector2 marioPos{ player.x, player.y };
+    Texture2D image = LoadTexture("Assets/Textures/Characters/Mario/Dk_Mario_Idle1.png");
+
+    Texture2D background = LoadTexture("Assets/stage1.png");
+
     SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
     //--------------------------------------------------------------------------------------
+
+    //Initialize DeltaTime
+    float timer = 0.0f;
+
+    //Initialize animation Frames (Player)
+    float animationTimer = 0.0f;
+    float animationSpeed = 0.15f;   // change frame every 0.15 seconds
+    int walkFrame = 0;
+
+    //Player Mov Bool
+    bool facingRight = true; // true = right, false = left
 
     // Main game loop
     while (!WindowShouldClose())    // Detect window close button or ESC key
@@ -55,18 +75,65 @@ int main(void)
         // Draw
         //----------------------------------------------------------------------------------
 
+        //Get DeltaTime
+        float deltaTime = GetFrameTime();  // time since last frame
+        animationTimer += deltaTime;
+        timer += deltaTime;
+
+        //init inside bool mov vars for player
+        bool PlayerisMoving = false;
 
         // moverse de derecha a izquierda
-        if (IsKeyDown(KEY_RIGHT)) player.x += playerSpeed;
-        if (IsKeyDown(KEY_LEFT)) player.x -= playerSpeed;
+        if (IsKeyDown(KEY_D)) {
+            player.x += playerSpeed;
+            PlayerisMoving = true;
+            facingRight = true; 
+        }
+
+        if (IsKeyDown(KEY_A)) {
+            player.x -= playerSpeed;
+            PlayerisMoving = true;
+            facingRight = false;
+        }
 
         // salto
 
-        if (IsKeyPressed(KEY_SPACE) && !isJumping)
+        if ((IsKeyPressed(KEY_SPACE) || IsKeyPressed(KEY_W)) && !isJumping)
         {
             velocityY = jumpForce;
             isJumping = true;
         }
+
+        //Playing Player Animations
+        if (isJumping) {
+            image = imgMarioJump;
+        }
+        else {
+            if (PlayerisMoving)
+            {
+                if (animationTimer >= animationSpeed)
+                {
+                    walkFrame++;
+                    if (walkFrame > 1) walkFrame = 0;  // only 2 frames (0 and 1)
+                    animationTimer = 0.0f;
+                }
+
+                if (walkFrame == 0)
+                    image = imgMarioWalk1;
+                else
+                    image = imgMarioWalk2;
+            }
+            else
+            {
+                image = imgMarioIdle;
+                walkFrame = 0;
+            }
+        }
+        
+        
+        
+        
+
 
         //poner gravedad
         velocityY += gravity;
@@ -98,9 +165,19 @@ int main(void)
         //poner suelo
         DrawRectangleRec(ground, DARKBROWN);
         //poner al jugador
-        DrawRectangleRec(player, RED);
+        DrawRectangleRec(player, {0,0,0,0});
 
         DrawText("Prueba de Donkey Kong_1", 10, 10, 20, WHITE);
+        //poner imagen
+        float scale = 2.0f;
+
+        Vector2 origin = { 0.0f, 0.0f };
+        Rectangle dest = { player.x , player.y ,image.width * scale, image.height * scale };
+        Rectangle src = { 0, 0, image.width, image.height };
+
+        if (!facingRight) src.width *= -1;
+
+        DrawTexturePro(image, src, dest, origin, 0.0f, WHITE);
 
         EndDrawing();
         //----------------------------------------------------------------------------------
@@ -108,6 +185,7 @@ int main(void)
 
     // De-Initialization
     //--------------------------------------------------------------------------------------
+    UnloadTexture(image);
     CloseWindow();        // Close window and OpenGL context
     //--------------------------------------------------------------------------------------
 
